e129fcf4f78f45d6fca834a963bbc5e6205d500d
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2022-12-09 17:14:59 +0200

    initializing data , images & design colors

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const eBackgroundColor = Colors.white;
+const ePrimaryColor = Color.fromRGBO(170, 109, 238,1.0);
+const eSecondaryColor = Color.fromRGBO(151, 255, 159,1.0);
+const eTextColor = Color.fromRGBO(66, 3, 70,1.0);
+const eTextLightColor = Color.fromRGBO(169, 140, 171,1.0);
+
+const eDefaultPadding = 20.0;
---
+++ lib/models/product.dart
@@ -0,0 +1,64 @@
+class Product {
+  final int? id,price;
+  final String? title,subtitle,desc,image;
+
+  Product({
+    this.id,
+    this.title,
+    this.subtitle,
+    this.desc,
+    this.price,
+    this.image,
+});
+}
+
+List<Product> products =[
+  Product(
+    id: 1,
+    title: "كاميرا مراقبة",
+    subtitle: "للبيوت والمكاتب",
+    desc: "موفرة للطاقة، دورا 180 درجة، تصوير قيديو ملون مع صوت مع إمكانية ربطها على تطبيق الموبايل وإمكانية التعرف على الوجوه",
+    price: 3999,
+    image: "assets/Camera.png",
+  ),
+  Product(
+    id: 2,
+    title: "ماكينة إزالة الشعر للسيدات",
+    subtitle: "حلاقة ناعمة وفعالة",
+    desc: "موفرة للطاقة، دوران للرأس 360 درجة، ناعمة على الجلد، مناسبة للبشرة الحساسة، نتائج تدوم وتدوم",
+    price: 1999,
+    image: "assets/Epilator.png",
+  ),
+  Product(
+    id: 3,
+    title: "سماعات رأس",
+    subtitle: "للموبايل واللابتوب",
+    desc: "دقة صوت عالية مع وصلة مسجل صوت، إمكانية رفع الصوت حتى 6 درجات",
+    price: 699,
+    image: "assets/HeadPhone.png",
+  ),
+  Product(
+    id: 4,
+    title: "لابتوب محمول",
+    subtitle: "للطلبة والأعمال المكتبية",
+    desc: "ذاكرة عشوائية 16 غيغا، مع سعة تخزين 1 تيرا وإمكانية التزويد بقرص SSD ، بطارية تدوم حتى 5 ساعات",
+    price: 1999,
+    image: "assets/Laptop.png",
+  ),
+  Product(
+    id: 5,
+    title: "جوال",
+    subtitle: "لتجربة ممتعة وفريدة",
+    desc: "احصل الآن على خصم 30% لفترة محدودة عند شرائك هذا الإصدار مع سوفتوير مجاني، العرض قائم حتى نفاذ الكمية",
+    price: 666,
+    image: "assets/SmartPhone.png",
+  ),
+  Product(
+    id: 6,
+    title: "ساعة ذكية",
+    subtitle: "ضد المياه",
+    desc: "تتميز بإمكانية ربطها مع الجوال وتلقي كافة الإشعارات والرد على الهاتف، متوفرة بألوان مختلفة",
+    price: 888,
+    image: "assets/SmartWatch.png",
+  ),
+];
